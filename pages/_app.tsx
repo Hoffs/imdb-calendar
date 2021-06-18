de0b7de19908b4cf6ef8ed4670ba2f1ddd34cd19
Ignas Maslinskas <ignasmaslinskas@gmail.com>
2021-06-18 12:53:31 +0300

    Initial configuration
    Add prettier, eslint, tailwind, jest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { GlobalStyles } from 'twin.macro';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import tw, { styled } from 'twin.macro';
 
-export default function Home() {
+const Header = styled.div(tw`font-mono text-sm text-gray-800`);
+
+export default function Home(): JSX.Element {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,9 +15,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <Header className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </Header>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -65,5 +68,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
